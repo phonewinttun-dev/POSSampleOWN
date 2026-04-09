db7f1ed1c03d71d3d43f1cb7ebfac6a930cbd358
theinhtetaung-dev <theinhtetaung.developer@gmail.com>
2026-04-09 14:24:54 +0630

    feat: add Sale Serrvice and SaleDTO

---
+++ POSSampleOWN.domain/DTOs/SaleDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using POSSampleOWN.database.Models;
+
+namespace POSSampleOWN.domain.DTOs;
+
+public class SaleDTO
+{
+    public long Id { get; set; }
+
+    public decimal TotalPrice { get; set; }
+
+    public string VoucherCode { get; set; } = null!;
+
+    public ICollection<Tbl_SaleItem> SaleItems { get; set; } = new List<Tbl_SaleItem>();
+}
+
+public class CreateSaleDTO
+{
+    List<CreateSaleItemDTO> Items { get; set; } = null!;
+}
+
+public class CreateSaleItemDTO
+{
+    public int ProductId { get; set; }
+    public int Quantity { get; set; }
+}
+
